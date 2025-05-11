a26a53568cc0af06e7ecf8654da626c938e77b91
Yogesh Singh Chilwal <yogeshsinghchilwal@gmail.com>
2025-05-11 13:52:42 +0530

    first commit - basic esp connection with rgb

---
+++ components/ColorPicker.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { ChromePicker } from "react-color";
+import { useState } from "react";
+
+export default function ColorPicker() {
+  const [color, setColor] = useState({ r: 0, g: 0, b: 0 });
+
+  // @ts-ignore
+  const sendColor = async (rgb) => {
+    try {
+      await fetch(`http://192.168.1.7/setColor?r=${rgb.r}&g=${rgb.g}&b=${rgb.b}`);
+    } catch (err) {
+      console.error("Failed to send color:", err);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <ChromePicker
+        color={color}
+        onChangeComplete={(color) => {
+          setColor(color.rgb);
+          sendColor(color.rgb);
+        }}
+      />
+      <div className="text-center">Selected: {color.r}, {color.g}, {color.b}</div>
+    </div>
+  );
+}
